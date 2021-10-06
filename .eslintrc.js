84dcfa324a12abd9a10c8b4815e9447278285153
jacket <1762982273@qq.com>
2021-10-06 11:37:21 +0800

    fix(lint-stage): fix lint-stage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
-  module.exports = {
+module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module",
+    parser: 'babel-eslint',
+    sourceType: 'module',
   },
   env: {
     browser: true,
     node: true,
     es6: true,
   },
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: ['plugin:@typescript-eslint/recommended'],
   globals: { _: true },
   rules: {
-    "prefer-rest-params": "off",
+    'prefer-rest-params': 'off',
     semi: [0],
-    "no-extra-semi": 2,
+    'no-extra-semi': 2,
   },
 }
-
