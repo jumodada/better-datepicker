e68ac521e8e4a838b0493395d37309b2ffdfcfd9
wukunyun <864353360@qq.com>
2022-07-15 17:01:32 +0800

    feat: reactive function not handle proxy target

--- packages/core/src/reactive/index.ts
+++ packages/core/src/reactive/index.ts
@@ -1,7 +1,12 @@
 import { mutableHandlers } from './handler'
 import { isObjectOrArray } from '../utils/typeOf'
 
+const proxyMap = new WeakMap()
+
 export function reactive(target: any): any {
+  if (proxyMap.has(target)) return proxyMap.get(target)
   if (!isObjectOrArray(target)) return target
-  return new Proxy(target, mutableHandlers)
+  const proxy = new Proxy(target, mutableHandlers)
+  proxyMap.set(target, proxy)
+  return proxy
 }
